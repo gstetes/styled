dfcf86dd641e5da3c97c5fcd02e18c7ca1ffb195
Guilherme Stetes <gstetes@gmail.com>
2020-06-18 14:51:07 -0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { Title, TitleSmall } from './styles'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title fontSize={20}>
+        Hello World!
+        <span>texto menor</span>
+      </Title>
+
+      <TitleSmall>Menor</TitleSmall>
     </div>
   );
 }
